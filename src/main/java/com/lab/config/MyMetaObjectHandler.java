d9b33cc8bf26c923ad204303562c8c2d701c707d
huruihan <15829390576@163.com>
2025-08-07 19:49:03 +0800

    第四次修改

--- src/main/java/com/lab/config/MyMetaObjectHandler.java
+++ src/main/java/com/lab/config/MyMetaObjectHandler.java
@@ -1,9 +1,12 @@
-package com.lab.handle;
+package com.lab.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
+@Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
--- src/main/java/com/lab/util/GlobalExceptionHandler.java
+++ src/main/java/com/lab/util/GlobalExceptionHandler.java
@@ -1,12 +1,29 @@
 package com.lab.util;
 
 import com.lab.vo.ResultVO;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//全局异常处理器
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    public ResultVO<?> handleValid(Exception e) {
+        String msg = "";
+        if (e instanceof MethodArgumentNotValidException ex) {
+            msg = ex.getBindingResult().getFieldError() == null ?
+                    "参数校验失败" :
+                    ex.getBindingResult().getFieldError().getDefaultMessage();
+        } else if (e instanceof BindException bx) {
+            msg = bx.getBindingResult().getFieldError() == null ?
+                    "参数校验失败" :
+                    bx.getBindingResult().getFieldError().getDefaultMessage();
+        }
+        return ResultVO.fail(msg);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResultVO<?> handle(Exception e) {
         return ResultVO.fail(e.getMessage());
